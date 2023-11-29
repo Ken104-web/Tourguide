41fe3883fa810c03d248076ee591b208c0a3ccc5
KenCorp <kennethwarui35@gmail.com>
2023-11-29 17:25:46 +0300

    done

--- client/src/search/search.jsx
+++ client/src/search/search.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./search.css";
-
 function Search() {
   const [issearch, setIsSearch] = useState(localStorage.getItem('isSearch') || '');
   const [data, setData] = useState([]);
@@ -23,33 +22,6 @@ function Search() {
     }
 }, [issearch]);
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const obj = JSON.stringify({ query: issearch });
-//         const response = await fetch("api/search", {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: obj,
-//         });
-
-//         if (response.ok) {
-//           const result = await response.json();
-//           console.log(result);
-//         } else {
-//           throw new Error('Failed to fetch data');
-//         }
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//         setData([]); 
-//       }
-//     };
-
-//     fetchData();
-//   }, [issearch]);
-
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -69,7 +41,6 @@ function Search() {
             value={issearch ?? ''}
             onChange={handleSearchSite}
           />
-          {/* You might add a submit button here if needed */}
         </form>
         <p>Most reached destinations across the World</p>
         <div className="map">
